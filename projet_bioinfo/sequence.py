c02f5d134b876829bfa84958654b568d6b5c6e71
study <study@debian.lip6>
2016-03-05 16:42:39 +0100

    05_03_16 samedi bu

--- projet_bioinfo/sequence.py
+++ projet_bioinfo/sequence.py
@@ -8,15 +8,15 @@ from math import *
 import numpy as np
 import matplotlib.pyplot as plt
 import collections
-#je suis un commentaire#
-alpha = {'A':1,'C':1,'D':1,'E':1,'F':1,'G':1,'H':1,'I':1,'K':1,'L':1,'M':1,'N':1,'P':1,'Q':1,'S':1,'T':1,'V':1,'W':1,'Y':1,'-':1}
+
+alpha = {'A':1,'C':1,'D':1,'E':1,'F':1,'G':1,'H':1,'I':1,'K':1,'L':1,'M':1,'N':1,'P':1,'Q':1,'R':1,'S':1,'T':1,'V':1,'W':1,'Y':1,'-':1}
 
 #lis le fichier Dtrain et enleve les commentaires
 #@return liste avec que les sequences
 def lire_texte():
     #contenu = ""
-    #contenu = open("Dtrain.txt", "r")
-    contenu = open("test_bis.txt", "r")
+    contenu = open("Dtrain.txt", "r")
+    #contenu = open("test_bis.txt", "r")
     #contenu = mon_fichier.read()
 
     ss_liste = ""
@@ -32,11 +32,25 @@ def lire_texte():
     contenu.close()
     return liste
 
-matrice = lire_texte()
-#print len(matrice)
+def lire_texte_bis(texte):
+    #contenu = ""
+    contenu = open(texte, "r")
+    #contenu = open("test_bis.txt", "r")
+    #contenu = mon_fichier.read()
 
-#print matrice 
-
+    ss_liste = ""
+    liste=[]
+#    i = 1
+    for i in contenu:
+        if (i[0] <> '>'):
+            ss_liste = ""
+            for char in i:
+                if (char <>'\n'):
+                    ss_liste = ss_liste + char
+            liste.append(ss_liste)
+    contenu.close()
+    return liste
+    
 # @param liste, plus exactement la liste qu'on a obtenue avec lire_texte()
 # @return une liste contenant plusieurs dictionnaires
 # chaque colonne a un dictionnaire : {acide aminé:nb occurence} 
@@ -53,8 +67,7 @@ def comparaison(liste):
         d.append(dico)
     return d
    
-c = comparaison(matrice)
-#print c
+
 
 # @param n = entier, correspondant au nb occurence de l'acide aminé
 # @return son poids 
@@ -62,7 +75,7 @@ def calPoids (n):
     w = n / (5643.0 + 21.0) # M = nb ligne sans commentaire et q = taille de l'alphabet 
     return w
     
-#print calPoids(c[47]['V'])
+
 
 #pour chaque colonne on calcule le poids de chaque acide aminé 
 def weight(liste):
@@ -77,9 +90,6 @@ def weight(liste):
         l2.append(poids)
     return l2
 
-we = weight(c)
-
-#print we
 
 # on calcule ici l'entropie relative d'une distribution d'une seule colonne
 def e_relative(p): 
@@ -96,9 +106,6 @@ def e_touteColonne(liste):
         liste_entropie.append(res)
     return liste_entropie # et les valeurs d'entropies sont stockées dans l'ordre
     
-liste_entropie = e_touteColonne(we)
-#print(liste_entropie) 
-#print(liste_entropie.index(max(liste_entropie)))
 
 #@param liste est une liste de dico contenant les poids de chaque acide amine
 def liste_argmax(liste): 
@@ -109,10 +116,47 @@ def liste_argmax(liste):
         #on stocke les lettres dominantes de chaque colonne dans la liste_max
         liste_max.append(lettre_max)
     return liste_max
-#print we[1]
-liste = liste_argmax(we)
-#print len(liste)
 
+
+
+def f0():  # fonction sans argument car "we" et "alpha" sont des variables globales 
+    liste_poids_moyen = {}
+    for acide in alpha.keys():
+        liste_poids_moyen[acide]=f0_bis(acide)
+        
+    return liste_poids_moyen
+
+
+def f0_bis(acidea):
+    sommepoids=0
+    #liste = we
+    for i in range (len(we)): # cle est un acide aminé 
+            sommepoids += we[i][acidea]
+    return sommepoids / len(we)
+
+#print liste_poids_moyen
+
+def eq(seq): 
+    res=0.
+    for i in range(len(seq)):
+        if(seq[i] in alpha):
+            frac = float(we[i][seq[i]])/liste_poids_moyen[seq[i]]
+            res += log(frac,2)
+    return res
+
+def ss_seq(texte):
+    liste = ""
+    res = []
+    contenu = lire_texte_bis(texte)[0]
+    #print "contenu est:"+str(contenu)
+    for i in range (len(contenu)-48):
+        liste = contenu[i:i+48]
+        #print liste
+        res.append(eq(liste))
+    return res
+    
+
+            
 #############################graphe###############################################
 def graphe():
     plt.xlabel(u'position i')
@@ -120,42 +164,27 @@ def graphe():
     plt.title(u"graphe représentant l'entropie relative en fonction de la position i")
     x=np.arange(48)
     plt.plot(x,liste_entropie)
-    #plt.show()
+    plt.show()
 #graphe()
+
+def g_vraissemblance():
+    plt.xlabel(u'position i')
+    plt.ylabel(u'log de vraisemblance')
+    plt.title(u"log de vraisemblance en fonction de sa première position i")
+    contenu = lire_texte_bis("test_seq.txt")[0]
+    x=np.arange(len(contenu)-48)
+    liste = ss_seq("test_seq.txt")
+    plt.plot(x,liste)
+    plt.show()
+g_vraissemblance()
 ##################################################################################
 
-#nb occurence de chaque lettre pour chaque alignement
-def f(liste):
-    d =[]
-    dico ={}
-    for i in range (len(liste)): 
-        dico = alpha.copy()     
-        for j in range (48):
-            for char in alpha.keys():
-                if (liste[i][j] == char):
-                   #print char
-                   dico[char]= dico[char] + 1
-        d.append(dico)
-    return d
-   
-freq = f(lire_texte())
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+#################################Variables globales######################    
+matrice = lire_texte()        
+c = comparaison(matrice)
+we = weight(c)
+liste_entropie = e_touteColonne(we)
+liste = liste_argmax(we)
+liste_poids_moyen =  f0()
+#print eq("PPAAAPQPKEPRYKALYDFAGQSAGELSLGKDEIILVTQKENNGWWLA")    
+#print(ss_seq("test_seq.txt"))
