38257046d16249066d44602108328eb746eecf27
MarièneWan <mariene.wan@laposte.fr>
2016-03-30 17:51:43 +0200

    debut internaute

--- Chaine_Markov/markov.py
+++ Chaine_Markov/markov.py
@@ -36,7 +36,7 @@ class Arc():
         
     def __str__(self):
         
-        string = "I'm a edge, from "+str(self.tail)+" to "+str(self.head)
+        string = "I'm an edge, from "+str(self.tail)+" to "+str(self.head)
         string += " with a proba " + str(self.prob)+"\n" 
         return string
      
@@ -52,10 +52,11 @@ class SimpleWeb():
         @param taille, la taille maximale du nombre de noeud dans le graphe
         la liste_node est une liste vide par défaut.
         """
-        
+        self.taille = taille
         self.liste_node = []
         self.matrice = np.zeros((taille,taille))
-        
+        for i in range(taille):
+            self.liste_node.append(Node(i))
         
     def __str__(self):
         
@@ -106,11 +107,17 @@ class SimpleWeb():
                 node.liste_ent.append(arc1)
         #updateProbas() 
         
+       # self.matrice[tail][head]=1
+        
+        
+        
+    def affiche (self):
+        print self.matrice
         
     def updateProbas(self):
         
         """
-        mettre à jour les probabilités suivant la proposition 
+        mettre à jour les probabilités suivant la matrice 
         faite par PageRank
         """        
         
@@ -120,3 +127,49 @@ class SimpleWeb():
                 proba = 1./len(node.liste_sort)
                 for arc in node.liste_sort:
                     arc.prob = proba
+                    self.matrice[arc.tail][arc.head] = proba
+        
+
+
+class Internaute():
+    
+    def __init__(self, web):
+        """
+        constructeur d'internaute, qui associe l'internaute au graph passé par argument
+        et garde sa position actuelle en attribut position 0 par défaut
+        """
+        
+        self.graph = web
+        self.pos = 0
+        self.cpt_node = []
+        for i in range(len(web.liste_node)):
+            self.cpt_node.append(0)
+        
+    def __str__(self):
+        return "I'm the robo, I have ",nbPas," steps"
+     
+    def goTo(self,node):
+        """
+        permet au robot de se positionner dans le noeud passé en paramètre
+        """
+        self.pos = node.id
+    
+    def trace(nbIte, filename):
+        """
+        internaute conserve les valeurs de epsilon
+        toutes les 100 iterations
+        dans ce fichier 
+        """        
+        
+        fichier = open(filename,"w")
+        
+        
+    def walk(nbPas, epsilon):
+        
+        for i in range(nbPas-1):
+            prev_pos = self.pos #position Ti
+            dis = web.matrice[prev_pos] #la distribution de proba de la position Ti
+            dis = [prob for ]
+            epsi = max(self.cpt_node)
+            if epsi <= epsilon:
+                break
