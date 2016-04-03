c85c6d32f0daa58c6bba98333049d4bc2cd7c177
study <study@debian.lip6>
2016-04-03 23:45:03 +0200

    try it , and test it with your own values good night

--- Chaine_Markov/markov.py
+++ Chaine_Markov/markov.py
@@ -61,6 +61,7 @@ class SimpleWeb():
         
     def __str__(self):
         
+        self.affiche()
         string = "I'm the graph, I have got "+str(len(self.liste_node))+" nodes.\n"
         string += " They are:\n"
         
@@ -129,6 +130,8 @@ class SimpleWeb():
                 for arc in node.liste_sort:
                     arc.prob = proba
                     self.matrice[arc.tail][arc.head] = proba
+            else:
+                self.matrice[node.id][node.id] = 1
         
 
 
@@ -141,27 +144,31 @@ class Internaute():
         """
         
         self.graph = web
-        self.pos = 0
+        self.pos = "undefined"
         #self.cpt_node = []
-        self.epsilon = 0.0
-        self.ti=[]
-        self.ti2=[]
-        self.diff=[]
-        
-        for i in range(len(web.liste_node)):
+        self.ti = []
+        self.ti2 = []
+        self.diff = []
+        self.liste_epsilon = [] #une liste qui contient les valeurs d'epsilon (superflu?)
+        for i in range(len(web.liste_node)): # initialise deux listes de compteurs à zeros
             self.ti.append(0)
             self.ti2.append(0)
         
+        
     def __str__(self):
-        return "I'm the robo, I have ",nbPas," steps"
+        return "I'm the robo, I have at the node "+str(pos)
      
-    def goTo(self,node):
+     
+    def goTo(self,nodeID):
         """
         permet au robot de se positionner dans le noeud passé en paramètre
         """
-        self.pos = node.id
+        self.pos = nodeID
         
-        self.ti2[node] = self.ti2[node]+1 # on incremente le compteur du noeud de 1
+        self.ti = np.copy(self.ti2)
+        self.ti2[nodeID] = self.ti2[nodeID]+1 # on incremente le compteur du noeud de 1
+        
+        #print "je suis a ",nodeID
     
     def trace(self,nbIte, filename):
         """
@@ -172,55 +179,65 @@ class Internaute():
         
         fichier = open(filename,"w")
         
+        for i in range(len(self.liste_epsilon)):
+            if i%nbIte == 0: 
+                value = self.liste_epsilon[i]
+                fichier.write(str(i)+" eme valeur d'epsilon est: "+str(value)+"\n")
+        
+        fichier.write("la derniere valeur est: "+str(self.liste_epsilon[len(self.liste_epsilon)-1]))
+        
+        fichier.close()
+        
     def epsilon(self):
+        """
+        calcule la valeur d'epsilon, la fonction est appelee a chaque deplacement de l'internaute
+        """
+    
+        somme1=np.sum(self.ti)
+        somme2=np.sum(self.ti2)            
+        #comme ti2 contient la liste de compteur à l'instant t+1, donc les deux sommes sont differentes
+
+        for i in range(len(self.ti2)):  
         
-        somme1=0
-        somme2=0
-        for i in range(len(self.ti2)):
-            somme1 = somme1+self.ti[i]
-            somme2 = somme2 + self.ti2[i]
+            self.diff.append(abs(float(self.ti[i])/somme1-float(self.ti2[i])/somme2)) 
+            #pour chaque noeud on calcule leur difference de compteur entre l'instant ti et ti+1
+        self.liste_epsilon.append(max(self.diff))
         
+        self.diff = []
         
-        self.diff.append(abs(self.ti[noeud]/somme1-self.ti2[noeud]/somme2))
-        
-        return max(self.diff)
+        return self.liste_epsilon[len(self.liste_epsilon)-1]
     
     
-    def walk(nbPas, e):
-        possibleNodes=[]
-        for i in range(nbPas-1):
+    def walk(self,nbPas, e):
+        if self.pos=="undefined":  # quand on appelle cette fonction juste apres la creation de l'objet
+            self.goTo(0)  # on met l'internaute sur le noeud 0 par defaut
             
-            possibleNodes = graph.matrice[self.pos] #une liste contenant les probas d'aller a un noeud i
+            
+        possibleNodes=[]
+        for i in range(nbPas):  # on s'arrete lorsque l'on a fait nbPas deplacement
+            
+            possibleNodes = self.graph.matrice[self.pos] #une liste contenant les probas d'aller a un noeud i
             
             possibleNodes = np.cumsum(possibleNodes) #transforme cette liste en une liste proba cumulee
             
             proba = random.random()
         
-            j=0
-
-            #prev_pos = self.pos #position Ti
-            while((j < (len(possibleNodes))) or (proba<possibleNodes[j])):
-                # j < len(possibleNodes) pour de boucler sur une liste contenant que des 0
-               j = j + 1
-
-            eps = epsilon(self.pos)   
-
-            goTo(j)
-
-            self.ti=self.ti2
+            goal = 0
             
-            if epsilon() <= e:
+            for goal in range(len(possibleNodes)):
+                if(proba>possibleNodes[goal]):
+                    goal = goal + 1
+                else:
+                    break
+                """ 
+                goal < len(possibleNodes) pour eviter de boucler sur une liste contenant que des 0 (qui ne doit pas se passer normalement)
+                a la fin de cette boucle, goal est le noeud ou l'internaute doit aller
+                """
+                
+               
+            
+            self.goTo(goal) #faire deplacer l'internaute
+            
+            if self.epsilon() <= e: #on s'arrete lorsqu'on a une convergence
                 break
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
--- Chaine_Markov/test.py
+++ Chaine_Markov/test.py
@@ -10,72 +10,63 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-node0 = Node(0)
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-node4 = Node(4)
-node5 = Node(5)
-node6 = Node(6)
-node7 = Node(7)
-node8 = Node(8)
-node9 = Node(9)
-
 graph = SimpleWeb(10)
-#graph.liste_node.append(node0)
-#graph.liste_node.append(node1)
-#graph.liste_node.append(node2)
-#graph.liste_node.append(node3)
-#graph.liste_node.append(node4)
-#graph.liste_node.append(node5)
-#graph.liste_node.append(node6)
-#graph.liste_node.append(node7)
-#graph.liste_node.append(node8)
-#graph.liste_node.append(node9)
 
-graph.AddArc(0,9)
-graph.AddArc(9,8)
-graph.AddArc(8,7)
-graph.AddArc(7,6)
-graph.AddArc(6,5)
-graph.AddArc(5,4)
-graph.AddArc(4,3)
-graph.AddArc(7,3)
-graph.AddArc(3,2)
+graph.AddArc(0,1)
+graph.AddArc(0,4)
+graph.AddArc(1,2)
 graph.AddArc(2,4)
-graph.AddArc(1,1)
-graph.AddArc(2,9)
-#graph.AddArc(1,5)
-#graph.AddArc(1,0)
-
+graph.AddArc(2,3)
+graph.AddArc(3,9)
+graph.AddArc(4,2)
+graph.AddArc(4,5)
+graph.AddArc(5,6)
+graph.AddArc(6,5)
+graph.AddArc(6,7)
+graph.AddArc(7,8)
+graph.AddArc(8,7)
 graph.updateProbas()
-graph.affiche()
+
 print graph
+
+bob = Internaute(graph)
+"""
+bob.goTo(3)
+print bob.ti, bob.ti2
+bob.goTo(9)
+print bob.ti, bob.ti2
+bob.goTo(8)
+print bob.ti, bob.ti2
+"""
+bob.walk(3000,0.0002)
+
+bob.trace(100,"epsilon_values.txt")
+
 
 ##############################################graphe########################################
 
-
-G = nx.DiGraph()
-G.add_nodes_from(graph.liste_node) # ajout de noeuds dans graph
-
-labels_nodes = {} #dico permettant de redefinir les labels de noeuds
-
-for node in G.nodes():
-    labels_nodes[node] = node.id
-
-H=nx.relabel_nodes(G,labels_nodes)     #graph H qui va s'afficher
-
-
-liste_edges = [] #variable contenant les arcs
-
-for node in graph.liste_node:
-    for arc in node.liste_sort:
-        liste_edges.append((arc.tail,arc.head)) 
-
-print liste_edges
-
-H.add_edges_from(liste_edges)
-nx.draw(H,with_labels=True)
+#
+#G = nx.DiGraph()
+#G.add_nodes_from(graph.liste_node) # ajout de noeuds dans graph
+#
+#labels_nodes = {} #dico permettant de redefinir les labels de noeuds
+#
+#for node in G.nodes():
+#    labels_nodes[node] = node.id
+#
+#H=nx.relabel_nodes(G,labels_nodes)     #graph H qui va s'afficher
+#
+#
+#liste_edges = [] #variable contenant les arcs
+#
+#for node in graph.liste_node:
+#    for arc in node.liste_sort:
+#        liste_edges.append((arc.tail,arc.head)) 
+#
+#print liste_edges
+#
+#H.add_edges_from(liste_edges)
+#nx.draw(H,with_labels=True)
 
 
 
